8c0e3eeb15b4529e223c44d515e2f6ff8793b748
quitam <tamphamasd@gmail.com>
2023-05-01 08:57:47 +0700

    setup api

---
+++ api/db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Abc12345",
+  database: "blog-app",
+});
---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.json("It work!!!!");
+});
+
+app.listen(8800, () => {
+  console.log("Connected");
+});

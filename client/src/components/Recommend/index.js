7f3e4de8b6c3481e159a465c845708644fb9dcaf
quitam <tamphamasd@gmail.com>
2023-05-04 23:13:33 +0700

    Fix recommend component

--- client/src/components/Recommend/index.js
+++ client/src/components/Recommend/index.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Recommend.module.scss';
 import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Recommend = ({ cat, post }) => {
     const [recommend, setRecommend] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -26,10 +28,14 @@ const Recommend = ({ cat, post }) => {
             {recommend.length > 0 &&
                 recommend.map((post) => (
                     <div className={cx('post')} key={post.id}>
-                        <img src={post.img} alt="" />
-                        <h2>{post.title}</h2>
+                        <Link to={`/post/${post.id}`}>
+                            <img src={post.img} alt="" />
+                        </Link>
+                        <Link to={`/post/${post.id}`}>
+                            <h2>{post.title}</h2>
+                        </Link>
                         <div className={cx('wrap-btn')}>
-                            <button>Read more</button>
+                            <button onClick={() => navigate(`/post/${post.id}`)}>Read more</button>
                         </div>
                     </div>
                 ))}

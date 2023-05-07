d6143c33e98fb101385898852415600380a649af
quitam <tamphamasd@gmail.com>
2023-05-08 05:56:32 +0700

    hosting mysql

--- api/db.js
+++ api/db.js
@@ -1,9 +1,16 @@
 import mysql from "mysql";
 
+// export const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Abc12345",
+//   database: "blog-app",
+//   insecureAuth: true,
+// });
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Abc12345",
-  database: "blog-app",
+  host: "b0nzlxgehf7m6yatmwqd-mysql.services.clever-cloud.com",
+  user: "upkibd3iupjkoqhb",
+  password: "sqzEz6qrcC1nt7Bwyf5x",
+  database: "b0nzlxgehf7m6yatmwqd",
   insecureAuth: true,
 });
